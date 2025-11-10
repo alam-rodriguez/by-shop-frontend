e31a0367674f89d19bc6ce26559f4aa99e314f8b
alamRodriguez <alamrd2016@gmail.com>
2025-11-10 10:18:19 -0400

    axios request udated

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:3001", // Cambia esto por la URL base real
+    baseURL: process.env.BACKEND_BASE_URL,
     withCredentials: true,
 });
 
