d74dc6de8a87a29ba3884e56afa145844e44fb77
alamRodriguez <alamrd2016@gmail.com>
2025-11-16 12:52:53 -0400

    sw-custom fixed

--- sw-custom.js
+++ sw-custom.js
@@ -1,29 +1,23 @@
-// Precaching (Workbox lo inyecta automáticamente)
-self.__WB_DISABLE_DEV_LOGS = true;
-
-self.addEventListener("install", () => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(self.clients.claim());
-});
+/* eslint-disable no-undef */
+// Next-PWA inyecta aquí el precache manifest
+precacheAndRoute(self.__WB_MANIFEST || []);
 
 // PUSH notifications
 self.addEventListener("push", (event) => {
     const data = event.data?.json() || {};
 
-    event.waitUntil(
-        self.registration.showNotification(data.title || "Nueva notificación", {
-            body: data.body,
-            icon: "/icons/icon-192x192.png",
-            badge: "/icons/icon-192x192.png",
-            data: { url: data.url || "/" },
-        })
-    );
+    const title = data.title || "Nueva notificación";
+    const options = {
+        body: data.body || "Tienes un nuevo mensaje",
+        icon: "/icons/icon-192x192.png",
+        badge: "/icons/icon-192x192.png",
+        data: { url: data.url || "/" },
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
 });
 
-// Click en notificación
+// Notification click
 self.addEventListener("notificationclick", (event) => {
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data.url));
