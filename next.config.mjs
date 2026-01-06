3eacb1f365eeea4ec48a468d97cfcc3a6c3a7893
alamRodriguez <alamrd2016@gmail.com>
2026-01-05 22:37:21 -0400

    Pwa options File Updated

--- next.config.mjs
+++ next.config.mjs
@@ -48,7 +48,14 @@ const withPWA = nextPWA({
     buildExcludes: [
         /app-build-manifest\.json$/, // <--- SOLUCIÓN DEL ERROR
         /sw-custom\.js$/, // <--- ya lo tenías para importar tu SW
+        /_next\/static\/.*/,
     ],
+    workboxOpts: {
+        exclude: [
+            /^\/_next\/static\//,
+            /\/_next\/static\/chunks\/.+\.js$/, // 🔹 evita precachear chunks con hash
+        ],
+    },
 });
 
 const nextConfig = {
